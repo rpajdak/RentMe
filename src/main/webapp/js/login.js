98592369a55e69f530f407baedf0d936302903f0
MiloszBryla <bryla.milosz@gmail.com>
2020-08-16 13:20:01 +0200

    add categories view

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,17 +1,20 @@
 
 function showLogin(){
 document.getElementById("sign-in").addEventListener("click", function () {
-    document.querySelector(".wrapper").style.opacity="0";
+    fadeOut("wrapper", 15)
+    document.querySelector(".wrapper").style.display="none";
     document.querySelector(".popup").style.display="flex";
+    fadeIn("popup", 35)
 })}
 
 function hideLogin(){
 document.querySelector(".close" ).addEventListener("click", function () {
-    document.querySelector(".wrapper").style.opacity="1";
+    document.querySelector(".wrapper").style.display="flex";
+    fadeIn("wrapper", 75)
     document.querySelector(".popup").style.display="none";
 
-})}
 
+})}
 
 function forgotPass(){
     document.getElementById("forg").addEventListener("click", function () {
@@ -28,3 +31,32 @@ function forgotPassHide(){
 
     })}
 
+ function fadeOut(element, duration) {
+     var i = 10;
+     var wrapper = document.getElementsByClassName(element)[0];
+     wrapper.style.opacity = 1;
+     var k = window.setInterval(function () {
+         if (i <= 0) {
+             clearInterval(k)
+             wrapper.style.opacity = 0;
+         } else {
+             wrapper.style.opacity = i / 10;
+             i--;
+         }
+     }, duration);
+ }
+
+ function fadeIn(element, duration) {
+     var i = 0;
+     var wrapper = document.getElementsByClassName(element)[0];
+     wrapper.style.opacity = 0;
+     var k = window.setInterval(function() {
+         if (i >= 10) {
+             clearInterval(k)
+             wrapper.style.opacity = 1;
+         } else {
+             wrapper.style.opacity = i / 10;
+             i++;
+         }
+     }, duration);
+ }
