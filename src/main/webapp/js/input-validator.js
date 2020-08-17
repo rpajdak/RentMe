318f28f9fcd947423d17c3c036e68c35d4b76b6d
Rafal <rpajdak@gmail.com>
2020-08-17 12:55:29 +0200

    Add validation for email when try to log in

--- src/main/webapp/js/input-validator.js
+++ src/main/webapp/js/input-validator.js
@@ -5,15 +5,16 @@ let second_passw_input = document.querySelector(".sec-passw");
 let resetPasswBtn = document.querySelector(".button");
 let name_input = document.querySelector(".name-surname");
 const submitBtn = document.querySelector(".submit-button");
+let loginBtn = document.querySelector(".loginBtn");
 let correctName = true;
 let correctEmail = true;
 
-function switchButton(id){
+function switchButton(id) {
     var button = document.getElementById(id);
     if (correctName && correctEmail) {
         console.log("enabled")
         button.disabled = false;
-    }else{
+    } else {
         console.log("disabled")
         button.disabled = true;
     }
@@ -24,46 +25,46 @@ function emailValidator() {
     let email_value = email_input.value;
     let validEmail = mailformat.test(email_value);
     const email_paragraph = document.querySelector(".warning-email");
-    console.log(validEmail)
-    if(!validEmail){
+    if (!validEmail) {
         email_paragraph.classList.remove("hidden");
         correctEmail = false;
-    }else{
+    } else {
         email_paragraph.classList.add("hidden");
         correctEmail = true;
     }
-    switchButton("btn-update");
+    // switchButton("btn-update");
 }
+
 function passwordValidator() {
     var passwFormat = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
     let passw_value = passw_input.value;
     let validPassw = passwFormat.test(passw_value);
-    if(!validPassw){
+    if (!validPassw) {
         document.querySelector(".warning-password").classList.remove("hidden");
-    }else{
+    } else {
         document.querySelector(".warning-password").classList.add("hidden");
     }
 }
 
-function checkMatchPasswords(){
+function checkMatchPasswords() {
     let passw_value = passw_input.value;
     let sec_passw = document.querySelector(".sec-passw").value;
     const warnig_passw_message = document.querySelector(".warning-equals");
-    if(passw_value !== sec_passw){
+    if (passw_value !== sec_passw) {
         warnig_passw_message.classList.remove("hidden");
-    }else{
+    } else {
         warnig_passw_message.classList.add("hidden")
     }
 }
 
-function nameValidator(){
+function nameValidator() {
     const regExForName = /^([a-zA-Z]{2,}\s[a-zA-z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/;
     let name_value = name_input.value;
     let validName = regExForName.test(name_value);
     alert("Test");
-    if(validName){
+    if (validName) {
         correctName = true;
-    } else{
+    } else {
         correctName = false;
     }
     displayWarning(validName);
@@ -71,33 +72,40 @@ function nameValidator(){
 }
 
 
-function displayWarning(validatorResult){
+function displayWarning(validatorResult) {
     if (validatorResult == false) {
         warning_paragraph.classList.remove("hidden");
     } else {
         warning_paragraph.classList.add("hidden");
     }
 }
-function checkEmail(){
-    email_input.addEventListener("focusout",emailValidator);
-}
-function checkName(){
-    name_input.addEventListener("mouseover",nameValidator);
+
+function checkEmail() {
+    loginBtn.addEventListener("mouseover", emailValidator);
 }
 
-function checkIfPasswordsAreEquals(){
-    resetPasswBtn.addEventListener("click",checkMatchPasswords);
-}
 
-function checkPasswordForRegEx(){
+function checkPasswordForRegEx() {
     passw_input.addEventListener("focusout",passwordValidator);
 }
 
+
+function checkName() {
+    name_input.addEventListener("mouseover", nameValidator);
+}
+
+
+function checkIfPasswordsAreEquals() {
+    resetPasswBtn.addEventListener("click", checkMatchPasswords);
+}
+
+
 function passwordClick() {
-    if(email_input) {
+    if (email_input) {
         email_input.addEventListener("click", emailValidator);
     }
 }
+
 
 function clickBtnResetPassw() {
     submitBtn.addEventListener("mouseover", emailValidator);
