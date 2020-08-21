f68891a23f722908a70d933c8a4b1772b45dea23
Rafal Pajdak <rpajdak@gmail.com>
2020-08-21 19:52:03 +0200

    Add method to get Admins or Renters.

--- src/main/java/com/codecool/controller/api/UserController.java
+++ src/main/java/com/codecool/controller/api/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class UserController {
@@ -17,19 +18,39 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/dummyAppUsers")
+    @GetMapping("/fillWithDummyAppUsers")
     @ResponseBody
     public void fillDbWithAppUsers() {
         userService.addUser(new AppUser("Joanna", "Brzuchacz", "jbrzuchacz@gmail.com", "asia1234", "Slusarska 9", "Krakow", true));
         userService.addUser(new AppUser("Michał", "Kuk", "mkuk@gmail.com", "michal1234", "Slusarska 9", "Krakow", true));
         userService.addUser(new AppUser("Miłosz", "Bryła", "mbryla@gmail.com", "milosz1234", "Slusarska 9", "Krakow", true));
         userService.addUser(new AppUser("Rafał", "Pajdak", "rpajdak@gmail.com", "rafal1234", "Slusarska 9", "Krakow", true));
+        userService.addUser(new AppUser("Maciek", "Bolek", "mbolek@gmail.com", "maciek1234", "Ceglarska 9", "Krakow", false));
+        userService.addUser(new AppUser("Julia", "Nowosad", "jnowosad@gmail.com", "julia1234", "Książnicza 9", "Krakow", false));
+        userService.addUser(new AppUser.Builder()
+                .firstName("Dorota")
+                .lastName("Bażant")
+                .email("dbazant@gmail.com")
+                .password("dorota1234")
+                .address("Karmelicka 1")
+                .city("Kraków")
+                .isAdmin(false)
+                .build());
     }
 
-
-    @GetMapping("/all")
+    @GetMapping("/getAllAdmins")
     @ResponseBody
-    public List<AppUser> getAllAppUsers() {
-        return userService.getAllAppUsers();
+    public List<AppUser> getAllAdmins() {
+        return userService.getAllAppUsers().stream()
+                .filter(AppUser::getIsAdmin)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/getAllRenters")
+    @ResponseBody
+    public List<AppUser> getAllRenters(){
+        return userService.getAllAppUsers().stream()
+                .filter(user -> !user.getIsAdmin())
+                .collect(Collectors.toList());
     }
 }
