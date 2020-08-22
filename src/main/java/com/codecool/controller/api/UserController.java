4efec959ab019c5a7b85da5b1b36c2a9245657a0
Rafal Pajdak <rpajdak@gmail.com>
2020-08-23 00:30:55 +0200

    Move filling of db with dummy content to sql file.

--- src/main/java/com/codecool/controller/api/UserController.java
+++ src/main/java/com/codecool/controller/api/UserController.java
@@ -19,26 +19,6 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/fillWithDummyAppUsers")
-    @ResponseBody
-    public void fillDbWithAppUsers() {
-        userService.addUser(new AppUser("Joanna", "Brzuchacz", "jbrzuchacz@gmail.com", "asia1234", "Slusarska 9", "Krakow", true));
-        userService.addUser(new AppUser("Michał", "Kuk", "mkuk@gmail.com", "michal1234", "Slusarska 9", "Krakow", true));
-        userService.addUser(new AppUser("Miłosz", "Bryła", "mbryla@gmail.com", "milosz1234", "Slusarska 9", "Krakow", true));
-        userService.addUser(new AppUser("Rafał", "Pajdak", "rpajdak@gmail.com", "rafal1234", "Slusarska 9", "Krakow", true));
-        userService.addUser(new AppUser("Maciek", "Bolek", "mbolek@gmail.com", "maciek1234", "Ceglarska 9", "Krakow", false));
-        userService.addUser(new AppUser("Julia", "Nowosad", "jnowosad@gmail.com", "julia1234", "Książnicza 9", "Krakow", false));
-        userService.addUser(new AppUser.Builder()
-                .firstName("Dorota")
-                .lastName("Bażant")
-                .email("dbazant@gmail.com")
-                .password("dorota1234")
-                .address("Karmelicka 1")
-                .city("Kraków")
-                .isAdmin(false)
-                .build());
-    }
-
     @GetMapping("/admins/all")
     @ResponseBody
     public List<AppUser> getAllAdmins() {
