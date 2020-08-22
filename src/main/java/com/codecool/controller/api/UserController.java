44485b90b7228f4391687309b823fa6b1a08f940
Rafal Pajdak <rpajdak@gmail.com>
2020-08-23 00:55:14 +0200

    Change return type in methods.

--- src/main/java/com/codecool/controller/api/UserController.java
+++ src/main/java/com/codecool/controller/api/UserController.java
@@ -1,6 +1,7 @@
 package com.codecool.controller.api;
 
-import com.codecool.model.AppUser;
+import com.codecool.converter.AppUserConverter;
+import com.codecool.modelDTO.AppUserDTO;
 import com.codecool.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,36 +9,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Controller
 public class UserController {
 
     private UserService userService;
+    private AppUserConverter appUserConverter;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AppUserConverter appUserConverter) {
         this.userService = userService;
+        this.appUserConverter = appUserConverter;
     }
 
     @GetMapping("/admins/all")
     @ResponseBody
-    public List<AppUser> getAllAdmins() {
-        return userService.getAllAppUsers().stream()
-                .filter(AppUser::getIsAdmin)
-                .collect(Collectors.toList());
+    public List<AppUserDTO> getAllAdmins() {
+        return appUserConverter.entitiesToDTO(userService.getAllAdmins());
     }
 
     @GetMapping("/renters/all")
     @ResponseBody
-    public List<AppUser> getAllRenters(){
-        return userService.getAllAppUsers().stream()
-                .filter(user -> !user.getIsAdmin())
-                .collect(Collectors.toList());
+    public List<AppUserDTO> getAllRenters() {
+        return appUserConverter.entitiesToDTO(userService.getAllRenters());
     }
+
     @GetMapping("/renters/{id}")
     @ResponseBody
-    public AppUser findUserById(@PathVariable("id") Long id){
-        return userService.getUserById(id);
+    public AppUserDTO findUserById(@PathVariable("id") Long id) {
+        return appUserConverter.entityToDTO(userService.getUserById(id));
     }
 
 }
