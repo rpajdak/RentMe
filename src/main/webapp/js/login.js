01b93b2f852dcd74ccf8383434d991ef2c7c06db
MiloszBryla <bryla.milosz@gmail.com>
2020-08-25 14:24:37 +0200

    login popup facelift

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -3,32 +3,34 @@ function showLogin(){
     document.getElementById("sign-in").addEventListener("click", function () {
         fadeOut("wrapper", 15)
         document.querySelector(".wrapper").style.display="none";
-        document.querySelector(".popup").style.display="flex";
-        fadeIn("popup", 35)
+        document.querySelector(".login-popup").style.display="flex";
+        fadeIn("login-popup", 35)
     })}
 
 function hideLogin(){
-    document.querySelector(".close" ).addEventListener("click", function () {
+    document.querySelector(".close-login" ).addEventListener("click", function () {
         document.querySelector(".wrapper").style.display="block";
         fadeIn("wrapper", 75)
-        document.querySelector(".popup").style.display="none";
-
-
+        document.querySelector(".login-popup").style.display="none";
     })}
 
 function forgotPass(){
     document.getElementById("forg").addEventListener("click", function () {
         document.querySelector(".wrapper").style.opacity="0";
-        document.querySelector(".popup").style.display="none";
-        document.querySelector(".popup2").style.display="flex";
+        fadeOut("login-popup", 15)
+        document.querySelector(".login-popup").style.display="none";
+        document.querySelector(".forgot-password-popup").style.display="flex";
+        fadeIn("forgot-password-popup", 15)
     })}
 
 function forgotPassHide(){
-    document.querySelector(".close2" ).addEventListener("click", function () {
+    document.querySelector(".close-password-recovery" ).addEventListener("click", function () {
+        fadeOut("forgot-password-popup", 15)
+        document.querySelector(".forgot-password-popup").style.display="none";
         document.querySelector(".wrapper").style.display="block";
         fadeIn("wrapper", 75)
-        document.querySelector(".popup").style.display="none";
-        document.querySelector(".popup2").style.display="none";
+/*        document.querySelector(".login-popup").style.display="none";*/
+
 
     })}
 
