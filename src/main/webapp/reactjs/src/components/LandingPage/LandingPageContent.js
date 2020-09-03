33804970f975c1dcc212a2e52797304ee5168e64
MiloszBryla <bryla.milosz@gmail.com>
2020-09-03 15:22:38 +0200

    fix categories view in landing page

--- src/main/webapp/reactjs/src/components/LandingPage/LandingPageContent.js
+++ src/main/webapp/reactjs/src/components/LandingPage/LandingPageContent.js
@@ -3,13 +3,19 @@ import "../../css/header-and-body.css";
 import "../../css/index.css";
 import logo from "../../assets/logo-with-slogan.svg"
 import scrollDownArrows from "../../assets/scroll-down-arrows.svg"
+import cameraJpg from "../../assets/camera.jpg"
+import homeEquipmentJpg from "../../assets/home-equipment.jpg"
+import adventureJpg from "../../assets/adventure.jpg"
+import scooterJpg from "../../assets/scooter.jpg"
+import instrumentsJpg from "../../assets/instruments.png"
+
 
 function LandingPageContent() {
     return (
         <div>
             <div className="landing-page-header">
                 <div className="menu">
-                    <ul>
+                    <ul>using javascript 
                         <li><a href="about.html">About us</a></li>
                         <li><a href="sign-up.html"><button type="button">Sign up</button></a></li>
                         <li><a href="#" id="sign-in">Sign in</a></li>
@@ -32,12 +38,11 @@ function LandingPageContent() {
                 <img id="scroll-down-arrows" src={scrollDownArrows}/>
             </div>
 
-
             <div id="categories-area">
                 <div className="categories">
-                    <div className="category" type="button" className="button12">
+                <div className="category">
                         <div className="category-image-container">
-                            <a href="#"><img className="category-image" src="../assets/camera.jpg"/></a>
+                            <a href="#"> <img className="category-image" src={cameraJpg}/></a>
                         </div>
                         <div className="category-name">
                             CAMERAS
@@ -45,7 +50,7 @@ function LandingPageContent() {
                     </div>
                     <div className="category">
                         <div className="category-image-container">
-                            <a href="#"><img className="category-image" src="../assets/home-equipment.jpg"/></a>
+                            <a href="#"><img className="category-image" src={homeEquipmentJpg}/></a>
                         </div>
                         <div className="category-name">
                             HOME EQUIPMENT
@@ -53,7 +58,7 @@ function LandingPageContent() {
                     </div>
                     <div className="category">
                         <div className="category-image-container">
-                            <a href="#"> <img className="category-image" src="../assets/adventure.jpg"/></a>
+                            <a href="#"> <img className="category-image" src={adventureJpg}/></a>
                         </div>
                         <div className="category-name">
                             ADVENTURE
@@ -61,7 +66,7 @@ function LandingPageContent() {
                     </div>
                     <div className="category">
                         <div className="category-image-container">
-                            <a href="#">   <img className="category-image" src="../assets/scooter.jpg"/></a>
+                            <a href="#">   <img className="category-image" src={scooterJpg}/></a>
                         </div>
                         <div className="category-name">
                             SCOOTERS
@@ -69,7 +74,7 @@ function LandingPageContent() {
                     </div>
                     <div className="category">
                         <div className="category-image-container">
-                            <a href="#">  <img className="category-image" src="../assets/instruments.png"/></a>
+                            <a href="#">  <img className="category-image" src={instrumentsJpg}/></a>
                         </div>
                         <div className="category-name">
                             INSTRUMENTS
@@ -82,10 +87,3 @@ function LandingPageContent() {
 }
 
 export default LandingPageContent;
-
-
-
-//
-//
-
-
