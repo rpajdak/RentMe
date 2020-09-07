6460fcb0262ab34fd070ee6ba6352a81a9632667
Rafal Pajdak <rpajdak@gmail.com>
2020-09-07 18:07:59 +0200

    Repair css and add validation.

--- src/main/webapp/reactjs/src/components/RecoverAcc/RecoverAcc.js
+++ src/main/webapp/reactjs/src/components/RecoverAcc/RecoverAcc.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom";
 import "../../css/header-and-body.css";
 import closeIcon from "../../assets/close-window.svg"
+import {useForm} from "react-hook-form";
 
 function forgotPassHide() {
     document.querySelector(".wrapper").style.display = "flex";
@@ -26,18 +27,35 @@ function fadeIn(element, duration) {
 }
 
 function RecoverAcc() {
+
+    const {register, handleSubmit, errors} = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data)
+    };
+
     return (
-        <div className="popup2">
-            <div className="popup-content2">
-                <h3>Enter your e-mail</h3>
-                <img className="close2" onClick={forgotPassHide} src={closeIcon}/>
-                <h4>If you have forgotten your account password please enter your e-mail
-                    and we will send you a password change code.</h4>
-                <h4 className="description">E-mail:</h4>
-                <input className="login-data-input2" type="text" placeholder="Enter your e-mail"/>
-                <button className="loginRecBtn"> Send me new password</button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="popup2">
+                <div className="popup-content2">
+                    <h3>Enter your e-mail</h3>
+                    <img className="close2" onClick={forgotPassHide} src={closeIcon}/>
+                    <h4>If you have forgotten your account password please enter your e-mail
+                        and we will send you a password change code.</h4>
+                    <h4 className="description">E-mail:</h4>
+                    <input className="login-data-input2" type="text" placeholder="Enter your e-mail" name="email"
+                           ref={register({
+                               required: "Required",
+                               pattern: {
+                                   value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                   message: "invalid email address"
+                               }
+                           })}/>
+                    {errors.email && <p>You need to enter email address.</p>}
+                    <button className="loginRecBtn"> Send me new password</button>
+                </div>
             </div>
-        </div>
+        </form>
     );
 }
 
