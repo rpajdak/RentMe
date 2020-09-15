fd388a53f78f332955f76fb38fd26f1de720ce3d
michal <kuk.michal3@gmail.com>
2020-09-15 21:33:51 +0200

    refactor css

--- src/main/webapp/reactjs/src/components/Payment/PaymentComponents/Payment.js
+++ src/main/webapp/reactjs/src/components/Payment/PaymentComponents/Payment.js
@@ -12,39 +12,39 @@ function Payment() {
                             <div className="col-50">
                                 <h3>Billing Address</h3>
                                 <label htmlFor="fname"><i className="fa fa-user"/> Full Name</label>
-                                <input type="text" id="fname" name="firstname" placeholder="John M. Doe"/>
+                                <input className="type-text" type="text" id="fname" name="firstname" placeholder="John M. Doe"/>
                                 <label htmlFor="email"><i className="fa fa-envelope"/> Email</label>
-                                <input type="text" id="email" name="email" placeholder="john@example.com"/>
+                                <input className="type-text" type="text" id="email" name="email" placeholder="john@example.com"/>
                                 <label htmlFor="adr"><i className="fa fa-address-card-o"/> Address</label>
-                                <input type="text" id="adr" name="address" placeholder="542 W. 15th Street"/>
+                                <input className="type-text" type="text" id="adr" name="address" placeholder="542 W. 15th Street"/>
                                 <label htmlFor="city"><i className="fa fa-institution"/> City</label>
-                                <input type="text" id="city" name="city" placeholder="New York"/>
+                                <input className="type-text" type="text" id="city" name="city" placeholder="New York"/>
 
                                 <div className="row">
                                     <div className="col-50">
                                         <label htmlFor="zip">Zip-Code</label>
-                                        <input type="text" id="zip" name="zip" placeholder="10001"/>
+                                        <input  className="type-text" type="text" id="zip" name="zip" placeholder="10001"/>
                                     </div>
                                 </div>
                             </div>
 
                             <div className="col-50">
                                 <h3 className="h3-text">Payment</h3>
-                                <label className="label" htmlFor="cname">Name on Card</label>
-                                <input type="text" id="cname" name="cardname" placeholder="John More Doe"/>
-                                <label className="label" htmlFor="ccnum">Credit card number</label>
-                                <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444"/>
-                                <label className="label" htmlFor="expmonth">Exp Month</label>
-                                <input type="text" id="expmonth" name="expmonth" placeholder="September"/>
+                                <label htmlFor="cname">Name on Card</label>
+                                <input className="type-text" type="text" id="cname" name="cardname" placeholder="John More Doe"/>
+                                <label htmlFor="ccnum">Credit card number</label>
+                                <input className="type-text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444"/>
+                                <label htmlFor="expmonth">Exp Month</label>
+                                <input className="type-text" type="text" id="expmonth" name="expmonth" placeholder="September"/>
 
                                 <div className="row">
                                     <div className="col-50">
                                         <label htmlFor="expyear">Exp Year</label>
-                                        <input type="text" id="expyear" name="expyear" placeholder="2018"/>
+                                        <input  className="type-text"  type="text" id="expyear" name="expyear" placeholder="2018"/>
                                     </div>
                                     <div className="col-50">
                                         <label htmlFor="cvv">CVV</label>
-                                        <input type="text" id="cvv" name="cvv" placeholder="352"/>
+                                        <input className="type-text" type="text" id="cvv" name="cvv" placeholder="352"/>
                                     </div>
                                 </div>
                             </div>
