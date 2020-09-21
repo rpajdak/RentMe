08d889c9d39c1d2c6ac7c703ea80c732a8db1ad8
Rafal Pajdak <rpajdak@gmail.com>
2020-09-21 22:37:30 +0200

    Add tests configuration

--- src/test/java/com/codecool/AppTestConfig.java
+++ src/test/java/com/codecool/AppTestConfig.java
@@ -6,6 +6,7 @@ import com.codecool.dao.UserRepository;
 import com.codecool.service.UserService;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,37 +16,20 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 public class AppTestConfig {
 
-//    @InjectMocks
-//    UserService userService;
-//
-//    @Mock
-//    UserRepository userRepository;
-//
-//    @Mock
-//    AppUserConverter appUserConverter;
-//
-//    @Bean
-//    public UserController userController() {
-//        return new UserController(userService, appUserConverter);
-//    }
-
-    @MockBean
-    UserRepository userRepository;
-
-    @MockBean
-    AppUserConverter appUserConverter;
-
     @Bean
-    public UserController userController(){
-        return new UserController(userService(), appUserConverter);
+    public AppUserConverter appUserConverter() {
+        return new AppUserConverter();
     }
 
     @Bean
-    public UserService userService(){
-        return new UserService(userRepository);
+    public UserController userController() {
+        return new UserController(userService(), appUserConverter());
     }
 
-
+    @Bean
+    public UserService userService() {
+        return Mockito.mock(UserService.class);
+    }
 
 
 }
