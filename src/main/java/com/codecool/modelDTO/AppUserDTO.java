3400e54a8d8a0f451d0cfaf55912eb81fcda069b
Rafal Pajdak <rpajdak@gmail.com>
2020-09-24 20:26:43 +0200

    Add builder for AppUserDTO

--- src/main/java/com/codecool/modelDTO/AppUserDTO.java
+++ src/main/java/com/codecool/modelDTO/AppUserDTO.java
@@ -15,4 +15,72 @@ public class AppUserDTO {
     private Double lat;
     private Double lng;
 
+    public static final class Builder {
+        private Long id;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String address;
+        private String city;
+        private Boolean isAdmin;
+        private Double lat;
+        private Double lng;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder id(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder city(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public Builder isAdmin(boolean isAdmin) {
+            this.isAdmin = isAdmin;
+            return this;
+        }
+
+        public Builder lat(Double lat) {
+            this.lat = lat;
+            return this;
+        }
+
+        public Builder lng(Double lng) {
+            this.lng = lng;
+            return this;
+        }
+
+        public AppUserDTO build() {
+            AppUserDTO appUserDTO = new AppUserDTO();
+            appUserDTO.id = this.id;
+            appUserDTO.firstName = this.firstName;
+            appUserDTO.lastName = this.lastName;
+            appUserDTO.address = this.address;
+            appUserDTO.city = this.city;
+            appUserDTO.lat = this.lat;
+            appUserDTO.lng = this.lng;
+            return appUserDTO;
+        }
+    }
 }
