3ea156711e4a72c133a82d846721becc90cfbb42
michal <kuk.michal3@gmail.com>
2020-09-24 13:44:42 +0200

    add tests for Item and DTO converter

--- src/test/java/com/codecool/AppTestConfig.java
+++ src/test/java/com/codecool/AppTestConfig.java
@@ -1,8 +1,15 @@
 package com.codecool;
 
+import com.codecool.controller.api.CategoriesController;
+import com.codecool.controller.api.ItemController;
 import com.codecool.controller.api.UserController;
 import com.codecool.converter.AppUserConverter;
+import com.codecool.converter.ItemConverter;
 import com.codecool.dao.UserRepository;
+import com.codecool.model.Category;
+import com.codecool.model.Item;
+import com.codecool.service.CategoryService;
+import com.codecool.service.ItemService;
 import com.codecool.service.UserService;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -22,8 +29,23 @@ public class AppTestConfig {
     }
 
     @Bean
+    public ItemConverter itemConverter(){
+        return new ItemConverter();
+    }
+
+    @Bean
     public UserController userController() {
         return new UserController(userService(), appUserConverter());
+    }
+
+    @Bean
+    public CategoriesController categoriesController(){
+        return  new CategoriesController(categoryService());
+    }
+
+    @Bean
+    public CategoryService categoryService(){
+        return Mockito.mock(CategoryService.class);
     }
 
     @Bean
@@ -31,7 +53,19 @@ public class AppTestConfig {
         return Mockito.mock(UserService.class);
     }
 
+    @Bean
+    public ItemService itemService(){
+        return Mockito.mock(ItemService.class);
+    }
 
+    @Bean
+    public Category category() { return Mockito.mock(Category.class);}
+
+
+    @Bean
+    public  ItemController itemController(){
+        return new ItemController(itemService(), itemConverter());
+    }
 }
 
 
